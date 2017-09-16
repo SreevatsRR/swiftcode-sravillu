90e386c48c1f4abaa2bc1f4291c5a9f5d2de2371
Sreevats R R <sreevatsrr@gmail.com>
2017-09-16 07:52:10 +0000

    Yo

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,4 +1,34 @@
 var app=angular.module('chatApp',['ngMaterial']);
 app.controller('chatController',function($scope){
+	
 
+	$scope.messages=[
+	{
+		sender:"BOT",
+		text:"Hi 1",
+		time:"1:12PM"
+	},
+	{ 
+		text:"Hi 2",
+		time:"1:13PM"
+
+	},
+	{
+		sender:"BOT",
+		text:"Hi 3",
+		time:"1:14PM"
+	},
+	{
+		text:"Hi 4",
+		time:"1:15PM"
+	}
+	];
+
+
+var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+exampleSocket.onmessage  =   function  (event) {
+       var jsonData = JSON.parse(event.data);
+       console.log(jsonData);
+   };
 });
+
